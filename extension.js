ba77c08e1cce3f42f8760e2f0a1929173a4e92ce
shaolibao <994917123@qq.com>
2021-01-21 16:28:54 +0800

    添加缓存

--- extension.js
+++ extension.js
@@ -7,14 +7,11 @@ const ProviderTemplate = require('./src/englishTemplate')
  */
 function activate(context) {
 	console.log('every-day-english插件已经被激活');
-	// vscode.window.setStatusBarMessage('你好，朋友！');
 	try {
-		const sidebarProvider = new ProviderTemplate()
+		const sidebarProvider = new ProviderTemplate(context)
 		let disposable = vscode.window.registerWebviewViewProvider("everyDayEnglishOne", sidebarProvider)
-		// let disposable2 = vscode.window.registerWebviewViewProvider("everyDayEnglishTwo", sidebarProvider)
 	
 		context.subscriptions.push(disposable);
-		context.subscriptions.push(disposable2);
 	} catch (error) {
 		window.showInformationMessage(error);
 	}
--- src/englishTemplate.js
+++ src/englishTemplate.js
@@ -1,41 +1,102 @@
 const vscode = require('vscode');
 const fetch = require('node-fetch')
 const template = require('./template')
-const cache = {}
+const fs = require("fs")
+const path = require("path")
 
+// 获取今日的日期y-m-d
+const getDate = (date = new Date()) => {
+  const day =date;
+  var current = "";
+  const Year= day.getFullYear();
+  const Month= day.getMonth()+1;
+  const Day= day.getDate();
+  current += Year + "-";
+  if (Month >= 10 ){
+    current += Month + "-";
+  }else {
+   current += "0" + Month + "-";
+  }
+  if (Day >= 10 ){
+   current += Day ;
+  } else {
+   current += "0" + Day ;
+  }
+  return current;
+}
 
 
 class ProviderTemplate {
-  constructor() {
+  constructor(context) {
     this.data = null
+    this.webview = null
+    this.storageUri = ""
+    this.cachePath = null
+    try {
+      console.log(context.storageUri);
+      if (context.storageUri && context.storageUri.path) {
+        // 优先读取缓存的数据
+        this.storageUri  = context.storageUri.path
+        if (!fs.existsSync(this.storageUri)) {
+          fs.mkdirSync(this.storageUri)
+        }
+        const pathCache = this.cachePath =  path.join(this.storageUri, 'cache.json')
+        if (!fs.existsSync(pathCache)) {
+          // 如果没有cache.json文件,则说明没有缓存
+          this.fetchData()
+          return this
+        }
+        const data = JSON.parse(fs.readFileSync(pathCache, 'utf8'))
+        if (getDate() === data.dailysentence.title) {
+          // 使用缓存数据
+          this.data  = data        
+        } else {
+          // 缓存已过期
+          this.fetchData()
+        }
+      } else {
+        this.fetchData()
+      }
+      
+    } catch (error) {
+      console.log(error);
+    }
   }
 	async resolveWebviewView(webviewView) {
-    const res = await this.fetch()
-    if (cache.now) {
-      this.data = cache.now
+    if (this.data) {
       this.render(webviewView, this.data)
     } else {
-      this.fetch().then(json => {
-        this.data = cache.now =  json
-        this.render(webviewView, json)
-      })
+      this.webview = webviewView
     }
+  }
+  fetchData() {
+    this.fetch().then(json => {
+      this.data =  json
+      if (this.webview) {
+        this.render(this.webview, this.data)
+      }
+    })
   }
   fetch() {
     try {
       return fetch('https://sentence.iciba.com/api/sentence/list?app_type=0&brand=apple&ck=&client=3&limit=1')
       .then(res => res.json())
       .then(json => {
-        console.log("数据请求成功!");
         const data = json.data || {}
         if (json.code === 0 && data.sentenceViewList && data.sentenceViewList.length) {
+          vscode.window.showInformationMessage(`最新每日英语(${data.sentenceViewList[0].dailysentence.title})已经更新!`)
+
+          if (!fs.existsSync(this.storageUri)) {
+            fs.mkdirSync(this.storageUri)
+          }
+          fs.writeFile(this.cachePath, JSON.stringify(data.sentenceViewList[0]), err => err)
           return data.sentenceViewList[0]
         } else {
-          window.showInformationMessage(json.msg);
+          vscode.window.showInformationMessage(json.msg);
         }
       });
     } catch (error) {
-      window.showInformationMessage(error);
+      vscode.window.showInformationMessage(error);
     }
   }
   render(webviewView, data=this.data) {
